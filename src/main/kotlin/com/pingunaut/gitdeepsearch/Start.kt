b8c5dfd67eced168ce93eb6f2c5c16781fe69a7e
Martin Spielmann <martin.spielmann@pingunaut.com>
2017-06-02 23:38:52 +0200

    First working version

--- src/main/kotlin/com/pingunaut/gitdeepsearch/Start.kt
+++ src/main/kotlin/com/pingunaut/gitdeepsearch/Start.kt
@@ -1,8 +1,70 @@
 package com.pingunaut.gitdeepsearch
 
+import org.eclipse.jgit.api.Git
+import java.nio.file.Files
+import org.eclipse.jgit.lib.ObjectId
+import java.nio.charset.StandardCharsets
+import java.nio.file.Path
+import java.nio.file.FileVisitOption
+import java.nio.file.Paths
+import java.util.stream.Collectors
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.lang.Exception
+import java.util.concurrent.Executors
+import org.eclipse.jgit.lib.ObjectReader
+
+@SpringBootApplication
 class Start {
-	
+
 	fun main(args: Array<String>) {
-		
+		if (args.size != 2) {
+			println("please provide a local repository path (1) and a search regex (2) as parameter")
+			System.exit(1);
+		}
+
+		println("Searching for ${args[1]} in ${args[0]}")
+
+		val localPath = Files.createTempDirectory("gitdeep");
+		val git = Git.cloneRepository().setURI(Paths.get(args[0]).toString()).setDirectory(localPath.toFile()).call();
+		// instead of checking all refs, we will walk over all object files to ensure that we'll find stuff which is no longer linked anywhere
+		unpackGitFiles(localPath)
+
+		val objectReader = git.repository.newObjectReader()
+
+		for (id in getIds(localPath)) {
+			checkId(id, args[1], objectReader)
+		}
+	}
+
+	fun getIds(repo: Path): List<String> {
+		//walk objects folder. build IDs using (two-digit) folder names + file names
+		return Files.walk(Paths.get(repo.toString(), ".git", "objects")).map { it.parent.fileName.toString() + it.fileName.toString() }.collect(Collectors.toList())
+	}
+
+	fun unpackGitFiles(repo: Path) {
+		// if git packed objects already, unpack them to make them searchable easily
+		val tmpPackFiles = Files.createTempDirectory(repo, "TMP_PACK_FILES")
+		Files.walk(Paths.get(repo.toString(), ".git/objects/pack")).filter { it.toString().endsWith(".pack") }.forEach {
+			val tmp = Paths.get(tmpPackFiles.toString(), it.getFileName().toString());
+			Files.move(it, tmp)
+			//TODO: beautify, add windows support... this is really ugly. jgit doesn't allow low level command unpack-objects... running it from command line here
+			ProcessBuilder().command("/bin/sh", "-c", String.format("git unpack-objects < %s", tmp.getFileName().toString())).directory(tmpPackFiles.toFile()).redirectErrorStream(true).start().waitFor();
+		}
+	}
+
+	fun checkId(id: String, searchTerm: String, objectReader: ObjectReader) {
+		try {
+			// load object by id
+			val objectLoader = objectReader.open(ObjectId.fromString(id))
+//			val type = objectLoader.getType(); 
+			val content = String(objectLoader.getBytes(), StandardCharsets.UTF_8)
+			// check if it contains the search term, if yes print it
+			if (content.contains(searchTerm)) {
+				println("${id}:\n ${content}");
+			}
+		} catch(e: Exception) {
+			//Just ignore for now
+			//e.printStackTrace();
+		}
 	}
 }
--- src/test/kotlin/com/pingunaut/gitdeepsearch/StartTest.kt
+++ src/test/kotlin/com/pingunaut/gitdeepsearch/StartTest.kt
@@ -2,10 +2,12 @@ package com.pingunaut.gitdeepsearch;
 
 import org.junit.Assert
 import org.junit.Test;
+import org.junit.Ignore
 
 class StartTest {
 
 	@Test
+	@Ignore
 	fun test() {
 		Assert.fail("Not yet implemented");
 	}
