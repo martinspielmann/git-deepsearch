964679d591340a371657c939df5fbfab48700323
Martin Spielmann <martin.spielmann@pingunaut.com>
2017-06-02 17:49:54 +0200

    initial commit. pom and main class

---
+++ src/main/kotlin/com/pingunaut/gitdeepsearch/Start.kt
@@ -0,0 +1,8 @@
+package com.pingunaut.gitdeepsearch
+
+class Start {
+	
+	fun main(args: Array<String>) {
+		
+	}
+}
---
+++ src/test/kotlin/com/pingunaut/gitdeepsearch/StartTest.kt
@@ -0,0 +1,13 @@
+package com.pingunaut.gitdeepsearch;
+
+import org.junit.Assert
+import org.junit.Test;
+
+class StartTest {
+
+	@Test
+	fun test() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
